5865bb31fe2f3b81dbdcad63de4e1a30d8234134
debu3223 <debabratamukherjee@trakinvest.com>
2020-12-10 12:10:53 +0530

    Paths Refractpred

--- subscriptionOrderDto.ts
+++ subscriptionOrderDto.ts
@@ -1,6 +1,6 @@
 // import { ChannelBillPlan } from "app/platform-3.0-Entities/channelBillPlan";
 import { DtoBase } from "../platform-3.0-Framework/entities/DtoBase";
-import { Subscription } from "rxjs";
+// import { Subscription } from "rxjs";
 import { ChannelBillPlanDto } from "./channelBillPlanDto";
 import { SubscriptionDto } from "./subscriptionDto";
 
