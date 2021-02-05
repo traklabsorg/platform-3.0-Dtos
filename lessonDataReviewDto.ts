2ac2b4836729e4a78ed929f1443638bc8e7d40d5
debu3223 <debabratamukherjee@trakinvest.com>
2021-02-05 20:28:36 +0530

    Changes sectionId to lessonDataId in LessonDataReview

--- lessonDataReviewDto.ts
+++ lessonDataReviewDto.ts
@@ -1,7 +1,6 @@
 import { DtoBase } from "./submodules/platform-3.0-Common/common/DtoBase";
 import { LessonDataDto } from "./lessonDataDto";
 import { UserDto } from "./userDto";
-import { LessonDataDto } from "./lessonDataDto";
 
 export class LessonDataReviewDto extends DtoBase{
     reviewDetails?: string;
