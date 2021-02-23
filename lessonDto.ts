ad4cfdcd7968359334897b4f9a77ef5366fd199a
debu3223 <debabratamukherjee@trakinvest.com>
2021-02-23 12:05:13 +0530

    channelSequenceId field added in channelDto

--- lessonDto.ts
+++ lessonDto.ts
@@ -14,6 +14,7 @@ export class LessonDto extends DtoBase {
   sectionId?: number;
   section?: SectionDto;
   collaborators?:number[];
+  // collaborators:string[];
   lessonData?: LessonDataDto[];
 }
 
