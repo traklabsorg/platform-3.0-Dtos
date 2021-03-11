c8e73aaef3b4dd87b92eff16eb57aa553de8e98c
AAYUSH SOMANI <aayush@trakinvest.com>
2021-03-11 20:53:58 +0530

    added json type for profileDto

--- profileDto.ts
+++ profileDto.ts
@@ -1,12 +1,25 @@
 import { DtoBase } from "./submodules/platform-3.0-Common/common/DtoBase";
 
-
-export class ProfileDto extends DtoBase{
+export class ProfileDto extends DtoBase {
   publishedCourses?: number;
   publishedLessons?: number;
   publishedWebinars?: number;
-  consumeCourses?: number;
+  consumedCourses?: number;
   consumedLessons?: number;
   consumedWebinars?: number;
-  channelPointsData?: string;
+  channelPointsData?: ChannelPointsData;
+}
+
+export class ChannelPointsData {
+  groupRank?: string;
+  totalPoints?: string;
+  ChannelData?: ChannelData[];
+}
+
+export class ChannelData {
+  channelId?: number;
+  channelImage?: string;
+  channelName?: string;
+  points?: number;
+  rank?: string;
 }
