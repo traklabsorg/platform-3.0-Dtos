5e667a8d4660415defe2e260329431e74a46c36f
debu3223 <debabratamukherjee@trakinvest.com>
2021-04-10 10:23:41 +0530

    added bill info dto

---
+++ billInfoDto.ts
@@ -0,0 +1,14 @@
+import { DtoBase } from './submodules/platform-3.0-Common/common/DtoBase';
+
+export class BillInfoDto extends DtoBase {
+    billId? : number;
+    billDate? : string;
+    title? : string;
+    description? : string;
+    totalUsers? : number;
+    perUserFee? : number;
+    currency? : string;
+    tax? : number;
+    totalCharges? : number;
+    totalrecords? : number;
+}
