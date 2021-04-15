1c7615dd274e3fd81ff564ee4b7f91dbf0bda83d
debu3223 <debabratamukherjee@trakinvest.com>
2021-04-15 11:12:34 +0530

    added receipt info dto

---
+++ receiptsInfoDto.ts
@@ -0,0 +1,8 @@
+import { DtoBase } from "./submodules/platform-3.0-Common/common/DtoBase";
+
+export class ReceiptsInfoDto extends DtoBase {
+    communityId? : number;
+	groupId? : number;
+	channelId? : number;
+}
+  
