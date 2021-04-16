0731698e101c21b06dceaff5691d1bed033069f0
debu3223 <debabratamukherjee@trakinvest.com>
2021-04-16 19:44:49 +0530

    changed receiptInfo dto

--- receiptsInfoDto.ts
+++ receiptsInfoDto.ts
@@ -9,5 +9,19 @@ export class ReceiptsInfoDto extends DtoBase {
 	channelName? : string; 
 	totalPay? : number; 
 	totalRecords? : number; 
+	firstname? : string;
+	lastname? : string;
+	timestamp? : string;
+	transactionId? : number;
+	stripeTransactionId? : number;
+	email? : string;
+	chargingType? : string;
+	channelFee? : number;
+	amountCollected? : number;
+	netPayout? : number;  
+	platformFee? : number;
+	stripeFees? : number;
+	currency? : string;
+	invoiceUrl? : string;
 }
   
